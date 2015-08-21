479af43ba0cc509485130f3fc953bd6385013518
Mantas Urnieža <mantas.urnieza@nfq.lt>
2015-08-21 15:03:14 +0300

    Settings added

---
+++ src/AppBundle/Admin/Setting.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Class Question
+ **/
+class Setting extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', ['label' => 'Setting name'])
+            ->add('value', 'text', ['label' => 'setting value'])
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('value')
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Settings.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Team
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Settings
+{
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
