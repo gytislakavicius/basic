0efc784b219000ff4aa6670cdd4aea1acd478207
Mantas Urnieža <mantas.urnieza@nfq.lt>
2015-08-21 11:02:03 +0300

    Save image to entity

--- src/AppBundle/Service/Users.php
+++ src/AppBundle/Service/Users.php
@@ -18,14 +18,22 @@ class Users
     /** @var Mailer */
     protected $mailer;
 
+    /** @var string */
+    protected $kernelDir;
+
+    /** @var string */
+    protected $domain;
+
     /**
      * @param UserManager $userManager
      * @param Mailer      $mailer
      */
-    public function __construct(UserManager $userManager, Mailer $mailer)
+    public function __construct(UserManager $userManager, Mailer $mailer, $kernelDir, $domain)
     {
         $this->userManager = $userManager;
-        $this->mailer = $mailer;
+        $this->mailer      = $mailer;
+        $this->kernelDir   = $kernelDir;
+        $this->domain      = $domain;
     }
 
     /**
@@ -44,6 +52,7 @@ class Users
             $userEntity->setUsername($user->username);
             $userEntity->setFullName($user->full_name);
             $userEntity->setIntranetId($user->intranet_id);
+            $userEntity->setPhotoUrl($this->domain . '/public/images/people/' . $user->username . '.jpg');
             // todo: change to normal emil
             $userEntity->setEmail($user->username . '@nfq.lt');
             $userEntity->setPlainPassword(uniqid());
@@ -53,7 +62,17 @@ class Users
                 $userEntity->setSuperAdmin(true);
                 $userEntity->setPlainPassword('admin');
             }
-
+// Uncomment following to download images
+//$photoUrl = 'https://people.nfq.lt/media/team/' . preg_replace('/^\/avatar\/30\/30\/([^.]*).*/', '$1', $user->photo->url) . '_190x190.jpg';
+//
+//try {
+//    file_put_contents($this->kernelDir . '/Resources/images/' . $user->username . '.jpg', file_get_contents($photoUrl));
+//    sleep(5);
+//    echo ".";
+//} catch (\Exception $e) {
+//    sleep(5);
+//    var_dump($photoUrl);
+//}
             $this->userManager->updateUser($userEntity);
         }
     }
