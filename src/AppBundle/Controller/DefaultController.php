fa73dd6c580ebe06ce28f11c53a7142e040a1ab1
Mantas Urnieža <mantas.urnieza@nfq.lt>
2015-09-02 19:46:07 +0300

    myTeam page added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -30,5 +31,28 @@ class DefaultController extends Controller
         }
 
         return $this->render('default/rules.html.twig');
+    }
+
+    /**
+     * @Route("/myTeam", name="myTeam")
+     */
+    public function myTeamAction()
+    {
+        if (!$this->isGranted(['IS_AUTHENTICATED_FULLY'])) {
+            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        }
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $team = $this->getDoctrine()->getRepository('AppBundle:Team')->findOneBy(['id' => $user->getTeam()]);
+
+        return $this->render(
+            'default/myTeam.html.twig',
+            [
+                'teamName'    => $team ? $team->getName() : "You have no team.",
+                'teamMembers' => $this->getDoctrine()->getRepository('AppBundle:User')->findBy(['team' => $user->getTeam()]),
+            ]
+        );
     }
 }
